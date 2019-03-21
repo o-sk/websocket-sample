0920c4f77e59716e8a5a784cc3739787838a2b72
o-sk <m.ohsuka@gmail.com>
2019-03-21 14:06:33 +0900

    import vue-native-websocket

--- front/src/main.ts
+++ front/src/main.ts
@@ -3,6 +3,13 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+// tslint:disable-next-line:no-var-requires
+const VueNativeSock = require("vue-native-websocket");
+
+Vue.use(VueNativeSock, "ws://localhost:8888", {
+  connectManually: true
+});
+
 Vue.config.productionTip = false;
 
 new Vue({
