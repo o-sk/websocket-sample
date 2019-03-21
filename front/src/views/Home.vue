25d46014a8049e7f98af2e156943d85adb7cbea3
o-sk <m.ohsuka@gmail.com>
2019-03-21 16:10:03 +0900

    Implement sending message via websocket

--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="home">
     <input v-model="text" type="text" />
+    <input type="button" @click="send" value="send" />
+    <p v-for="(message, i) in receiveMessages" :key="message + i">
+      {{ message }}
+    </p>
   </div>
 </template>
 
@@ -10,5 +14,27 @@ import { Component, Vue } from "vue-property-decorator";
 @Component({})
 export default class Home extends Vue {
   public text: string = "";
+  public receiveMessages: string[] = [];
+  public socket: WebSocket = new WebSocket("ws://localhost:8888/echo");
+
+  public mounted(): void {
+    this.socket.onopen = () => {
+      console.log("ws open");
+    };
+    this.socket.onmessage = e => {
+      this.receiveMessages.push(e.data);
+    };
+    this.socket.onclose = e => {
+      console.log("ws close");
+    };
+  }
+
+  public send(): void {
+    this.socket.send(this.text);
+  }
+
+  public beforeDestroy(): void {
+    this.socket.close();
+  }
 }
 </script>
